3ffbc396320c37759be48b250c6273933002c717
Kaijin Chen <chenkj9@mail2.sysu.edu.cn>
2024-09-15 16:26:14 +0800

    config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,11 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  base: "sysutkd2003",
   bundler: viteBundler(),
   theme: defaultTheme(),
+
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
 })
